56d15ccf1b03f20d42c56807fe7ddfad73cc861e
Aaron <50504602+xAaronWx@users.noreply.github.com>
2021-04-01 16:30:01 -0400

    Changes

--- src/Components/PokeFetch/PokeFetch.js
+++ src/Components/PokeFetch/PokeFetch.js
@@ -1,52 +1,3 @@
-// import React, { Component } from "react";
-// import "./PokeFetch.css";
-
-// class PokeFetch extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       pokeInfo: "",
-//       pokeSprite: "",
-//       pokeName: "",
-//     };
-//   }
-
-//   fetchPokemon() {
-//     let min = Math.ceil(1);
-//     let max = Math.floor(152);
-//     let pokeNum = Math.floor(Math.random() * (max - min) + min);
-//     fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNum}`, {
-//       method: "GET",
-//     })
-//       .then((res) => res.json())
-//       .then((res) => {
-//         this.setState({
-//           pokeInfo: res,
-//           pokeSprite: res.sprites.front_default,
-//           pokeName: res.species.name,
-//         });
-//       })
-//       .catch((err) => console.log(err));
-//   }
-
-//   render() {
-//     return (
-//       <div className={"wrapper"}>
-//         <button className={"start"} onClick={() => this.fetchPokemon()}>
-//           Start!
-//         </button>
-//         <h1 className={"timer"}>Timer Display</h1>
-//         <div className={"pokeWrap"}>
-//           <img className={"pokeImg"} src={this.state.pokeSprite} />
-//           <h1 className={"pokeName"}>{this.state.pokeName}</h1>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default PokeFetch;
-
 import React, { Component } from "react";
 import "./PokeFetch.css";
 
@@ -125,7 +76,7 @@ class PokeFetch extends Component {
                 ? { filter: "brightness(100%)" }
                 : { filter: "brightness(0%)" }
             }
-            alt="/"
+            alt=""
           />
           <h1
             className={"pokeName"}
